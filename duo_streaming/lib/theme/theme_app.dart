9325055f839702819a10ba9a5974b58ecdbaf7fd
Pranjal <pranjalbharat07@gmail.com>
2023-11-26 16:52:46 +0530

    fixed file issue

--- duo_streaming/lib/theme/theme_app.dart
+++ duo_streaming/lib/theme/theme_app.dart
@@ -1,3 +1,1 @@
-import 'package:flutter/material.dart';
-
-const PRIMARY_COLOR = Color.fromARGB(255, 22, 74, 116);
+// TODO Implement this library.
---
+++ lib/theme/theme_app.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color.fromARGB(255, 22, 74, 116);
